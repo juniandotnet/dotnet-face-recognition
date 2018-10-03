9d1a92a6cdc91ed2ad45c9cafaee264de67deff2
Junian Triajianto <author@junian.net>
2018-10-04 06:39:15 +0700

    Init from eto

---
+++ FaceRecognition.Gtk/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+
+namespace FaceRecognition.Gtk
+{
+	class MainClass
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			new Application(Eto.Platforms.Gtk2).Run(new MainForm());
+		}
+	}
+}
